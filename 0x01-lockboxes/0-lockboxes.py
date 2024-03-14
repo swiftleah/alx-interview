4de43fa41c4e3805e43a6459f3830300938d34dc
swiftleah <leahrosehenry@gmail.com>
2024-03-14 12:28:00 +0000

    Task 0

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" determines if all boxes can be opened """
+
+def canUnlockAll(boxes):
+    """ start of code """
+    visited = set()
+    visited.add(0)
+
+    stack = [0]
+
+    while stack:
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key not in visited and key < len(boxes):
+                stack.append(key)
+                visited.add(key)
+
+    return len(visited) == len(boxes)
+
+if __name__ == "__main__":
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))
+
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))
+
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))
