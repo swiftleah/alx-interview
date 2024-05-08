6f1b9354413630cb55f17f85c52ebc8c08955677
swiftleah <leahrosehenry@gmail.com>
2024-05-08 13:32:35 +0000

    Task 0 - different method

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -5,18 +5,18 @@
 def island_perimeter(grid):
     """ function returns perimeter of island in grid
     represented by 1's """
-    oneCount = 0
+    perimeter = 0
 
-    for row in grid:
-        for element in row:
-            if element == 1:
-                oneCount += 1
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == len(grid[0]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
 
-    if oneCount == 0:
-        return 0
-
-    if oneCount == 1:
-        result = 4
-    else:
-        result = ((oneCount * 2) + 2)
-    return result
+    return perimeter
